f48452d86ae234279739f4ab71d353bd91e71b59
Mateusz Sokola <mateuszsokola@gmail.com>
2021-09-04 22:06:19 +0400

    docs(Board): add code comments

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -11,9 +11,12 @@ type Props = {
 };
 
 export const Board = ({ tiles, tileCountPerRow = defaultTileCount }: Props) => {
+  // container width = tile width * tile count per row
   const containerWidth = tileTotalWidth * tileCountPerRow;
+  // board width = container width + margin
   const boardWidth = containerWidth + boardMargin;
 
+  // render all tiles on the board
   const tileList = tiles.map(({ id, ...restProps }) => (
     <Tile key={`tile-${id}`} {...restProps} zIndex={id} />
   ));
