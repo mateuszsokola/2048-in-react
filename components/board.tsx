66f16e584f5b0a8941905e22e6c7b8b5807664f3
mateusz <mateusz@stake.fish>
2023-10-01 14:19:23 +0400

    feat: add tile and tile container

--- components/board.tsx
+++ components/board.tsx
@@ -1,12 +1,13 @@
+import Tile from "@/components/tile";
 import styles from "@/styles/board.module.css";
 
 export default function Board() {
   const renderGrid = () => {
     const cells: JSX.Element[] = [];
-    const totalCellCount = 16;
+    const totalCellsCount = 16;
 
-    for (let index = 0; index < totalCellCount; index += 1) {
-      cells.push(<div key={`${index}`} className={styles.cell} />);
+    for (let index = 0; index < totalCellsCount; index += 1) {
+      cells.push(<div className={styles.cell} key={index} />);
     }
 
     return cells;
@@ -14,6 +15,9 @@ export default function Board() {
 
   return (
     <div className={styles.board}>
+      <div className={styles.tiles}>
+        <Tile />
+      </div>
       <div className={styles.grid}>{renderGrid()}</div>
     </div>
   );
