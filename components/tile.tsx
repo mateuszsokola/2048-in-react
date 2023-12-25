149da2cbd211eb340056f6ed19e38efc5621b491
mateusz <mateusz@stake.fish>
2023-12-25 19:29:46 +0100

    feat: add desktop styles

--- components/tile.tsx
+++ components/tile.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import {
-  containerWidth,
+  containerWidthMobile,
+  containerWidthDesktop,
   mergeAnimationDuration,
   tileCountPerDimension,
 } from "@/constants";
@@ -9,6 +11,11 @@ import styles from "@/styles/tile.module.css";
 import usePreviousProps from "@/hooks/use-previous-props";
 
 export default function Tile({ position, value }: TileProps) {
+  const isWideScreen = useMediaQuery({ minWidth: 512 });
+  const containerWidth = isWideScreen
+    ? containerWidthDesktop
+    : containerWidthMobile;
+
   const [scale, setScale] = useState(1);
   const previousValue = usePreviousProps<number>(value);
   const hasChanged = previousValue !== value;
