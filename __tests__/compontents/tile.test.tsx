d728cb448de55d120ea7024266426ddd4432e627
mateusz <mateusz@stake.fish>
2023-12-25 19:08:46 +0100

    feat: add tile animations

---
+++ __tests__/compontents/tile.test.tsx
@@ -0,0 +1,61 @@
+import { render, waitFor } from "@testing-library/react";
+import Tile from "@/components/tile";
+
+describe("Tile", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  it("should render with correct position and value", () => {
+    // Arrange
+    const position: [number, number] = [1, 2];
+    const value = 2048;
+
+    // Act
+    const { container } = render(<Tile position={position} value={value} />);
+
+    const tile: HTMLDivElement = container.firstChild as HTMLDivElement;
+    expect(tile.textContent).toEqual("2048");
+    expect(tile.className).toEqual("tile");
+    expect(tile).toHaveStyle({
+      left: `72px`,
+      top: `144px`,
+      zIndex: `2048`,
+    });
+  });
+
+  it("should apply animation when value changes", async () => {
+    // Arrange
+    const position: [number, number] = [0, 0];
+    const initialValue = 2;
+    const updatedValue = 4;
+
+    // Act
+    const { getByText, rerender } = render(
+      <Tile position={position} value={initialValue} />,
+    );
+
+    // Assert initial state
+    const tileElement = getByText(`${initialValue}`);
+    expect(tileElement).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(tileElement).toHaveStyle({
+        transform: "scale(1)",
+      });
+    });
+    rerender(<Tile position={position} value={updatedValue} />);
+
+    await waitFor(() => {
+      expect(tileElement).toHaveStyle({
+        transform: "scale(1.1)",
+      });
+    });
+
+    await waitFor(() => {
+      expect(tileElement).toHaveStyle({
+        transform: "scale(1)",
+      });
+    });
+  });
+});
--- constants.ts
+++ constants.ts
@@ -1,3 +1,7 @@
 export const tileCountPerDimension = 4;
 
 export const containerWidth = 288; // pixels
+
+export const mergeAnimationDuration = 100; // ms
+
+export const moveAnimationDuration = 200; // ms
