88a4176858d5072a77787963a82b8e29b1746daf
mateusz <mateusz@stake.fish>
2023-12-25 19:18:07 +0100

    feat: add scoring

--- __tests__/context/game-context.test.tsx
+++ __tests__/context/game-context.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render } from "@testing-library/react";
-import GameProvider from "@/context/game-context";
 import Board from "@/components/board";
+import Score from "@/components/score";
+import GameProvider from "@/context/game-context";
 
 describe("GameProvider", () => {
   describe("startGame", () => {
@@ -24,6 +25,37 @@ describe("GameProvider", () => {
       );
 
       expect(container.querySelectorAll(".tile")).toHaveLength(2);
+    });
+  });
+
+  describe("score", () => {
+    it("should return score", () => {
+      const { container } = render(
+        <GameProvider>
+          <Score />
+          <Board />
+        </GameProvider>,
+      );
+
+      expect(container.querySelector(".score > div")?.textContent).toEqual("0");
+    });
+
+    it("should refresh score after move", () => {
+      const { container } = render(
+        <GameProvider>
+          <Score />
+          <Board />
+        </GameProvider>,
+      );
+
+      expect(container.querySelector(".score > div")?.textContent).toEqual("0");
+
+      fireEvent.keyDown(container, {
+        key: "ArrowUp",
+        code: "ArrowUp",
+      });
+
+      expect(container.querySelector(".score > div")?.textContent).toEqual("4");
     });
   });
 });
