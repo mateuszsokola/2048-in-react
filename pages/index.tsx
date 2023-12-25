d5a9d922772a17e5a48d0ef0806ff49d8568ab59
mateusz <mateusz@stake.fish>
2023-12-25 21:25:22 +0100

    feat: add meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,20 @@ export default function Home() {
   return (
     <div className={styles.twenty48}>
       <Head>
-        <title>Play 2048!</title>
-        <meta name="description" content="2048 in React" />
+        <title>Play 2048</title>
+        <meta
+          name="description"
+          content="Fully-functional 2048 game built in NextJS and TypeScript. Including animations."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon16.png" />
       </Head>
       <header>
         <h1>2048</h1>
